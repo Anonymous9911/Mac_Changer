abe58eb05fb3680e0c7c718d9058559f2d998d89
Anonymous9911 <100386180+Anonymous9911@users.noreply.github.com>
2022-02-26 19:57:45 +0300

    Update Mac_Changer.py

--- Mac_Changer.py
+++ Mac_Changer.py
@@ -1,23 +1,25 @@
-#! usr/bin/env python
+#!/usr/bin/env python
 
 import subprocess
 import optparse
 import re
 
+
 def get_arguments():
     parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help="Interface To Change Its Mac Address")
-    parser.add_option("-m", "--macaddress", dest="new_mac", help="New Mac Address")
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
     (options, arguments) = parser.parse_args()
     if not options.interface:
-        parser.error("[-] Please specify an interface, use --help for more info. ")
+        parser.error("[-] Please Specify an Interface, use --help for more info.")
     elif not options.new_mac:
-        parser.error("[-] Please specify a new mac, use --help for more info. ")
-        return options
+        parser.error("[-] Please Specify a new mac, use --help for more info.")
+    return options
+
 
 def change_mac(interface, new_mac):
-    print("[+] Changing Mac Address for " + interface + " to " + new_mac)
-    subprocess.call(["ifconfig", interface, "down,"])
+    print("[+] Changing MAC Address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
@@ -25,12 +27,10 @@ def get_current_mac(interface):
     ifconfig_result = subprocess.check_output(["ifconfig", interface])
 
     mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
-
     if mac_address_search_result:
-        return mac_address_search_result(0)
+        return mac_address_search_result.group(0)
     else:
-        print("[+] Could not read MAC address")
-
+        print("[-] Could not read MAC address. ")
 
 
 options = get_arguments()
@@ -45,4 +45,3 @@ if current_mac == options.new_mac:
     print("[+] MAC address was successfully changed to " + current_mac)
 else:
     print("[-] MAC address did not change. ")
-
